4507bcfdd6c67339ea90a94f6c0540e5d6f40632
江嘉伟 <jiangjiawei@jiangjiaweideMacBook-Pro.local>
2021-09-12 23:48:48 +0800

    fix logger conf path

--- core/callgraphs/cg_test.go
+++ core/callgraphs/cg_test.go
@@ -35,17 +35,28 @@ func TestBuildCallGraph	(t *testing.T) {
 
 		callgraph.GraphVisitEdges(result.CallGraph, func(edge *callgraph.Edge) error {
 			if edge.Site == nil {
-				log.Infof("%s",edge.String())
+				//log.Infof("%s",edge.String())
 			}else{
-
-				//log.Debugf(edge.Site.String())
-				if strings.Contains(edge.Callee.String(),"fabric"){
-					log.Infof("fabric pkg callee:%s", edge.Callee.String())
-				}
-				if strings.Contains(edge.Callee.String(),"PutState")  {
+				if strings.Contains(edge.Callee.String(),"PutState") || strings.Contains(edge.Caller.String(),"PutState") {
 					log.Infof("putState Caller: %s", edge.Caller.String())
+					log.Infof("putState Callee: %s", edge.Callee.String())
 				}
+
+				if edge.Site != nil && edge.Site.Common().StaticCallee() == nil{
+					if strings.Contains(edge.Site.Common().Method.String(), "PutState"){
+						log.Infof("dynamic call: %s, caller:%s, callee:%s", edge.Site.Common().Method.FullName(),edge.Caller.String(),edge.Callee.String())
+					}
+				}
+				//if edge.Site.Common().StaticCallee() == nil{
+				//	log.Infof("dynamic call:%s", edge.Site.String())
+				//}
+				////log.Debugf(edge.Site.String())
+				//if strings.Contains(edge.Callee.String(),"fabric"){
+				//	log.Infof("fabric pkg callee:%s", edge.Callee.String())
+				//}
+
 			}
+
 			return nil
 		})
 
