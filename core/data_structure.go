1e1c7f2e8f22477d629042773b00d8bab1d7e58f
JarvistFth <Jarvist_fth@126.com>
2021-10-19 01:13:02 +0800

    update read-after-write

--- core/data_structure.go
+++ core/data_structure.go
@@ -61,3 +61,33 @@ func (m ReadAfterWriteMap) Contains(c ssa.CallInstruction) (rw RwDetails, ok boo
 	rw,ok = m[c]
 	return rw,ok
 }
+
+type LatticeTag struct {
+	tag    string
+	msgSet map[string]bool
+}
+
+func (t *LatticeTag) Add(tag string) {
+	if t.msgSet == nil{
+		t.msgSet = make(map[string]bool)
+	}
+	if _,ok := t.msgSet[tag];ok{
+		return
+	}
+
+	t.tag += tag + " | "
+	t.msgSet[tag] = true
+}
+
+func (t *LatticeTag) Contains(tag string) bool {
+	_,ok :=t.msgSet[tag]
+	return ok
+}
+
+func (t *LatticeTag) Delete(tag string) {
+	delete(t.msgSet,tag)
+}
+
+func (t *LatticeTag) String() string {
+	return t.tag
+}
